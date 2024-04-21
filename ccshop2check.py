425634c047789a786f240ba0ac5ffed4b7825b6b
rip <49995327+rip@users.noreply.github.com>
2024-04-21 12:11:08 -0500

    Update ccshop2check.py

--- ccshop2check.py
+++ ccshop2check.py
@@ -1,3 +1,5 @@
+#⚙️ Format:   CCnum|Exp.Date|CVV|Address|Zip|
+#🎛️ Mode: NO CHARGE
 import tkinter as tk
 from tkinter import simpledialog
 ー=tk.Tk();ー.title('🦎')
@@ -5,6 +7,7 @@ def ァ():
     ア = simpledialog.askstring("ccshop2check", "paste the html from view-source:https://ccshop2.com/cc_list.php (check your terminal for the output)")
     if ア:
        イ = ア.split('\n')
+       valid = []; invalid = []
        for ウ in イ:
            if '<td class="text-start">' in ウ:
             card = ウ.split('<td class="text-start">')[1].split('|')
@@ -16,9 +19,15 @@ def ァ():
                if any(i.isdigit() for i in card[6]):
                   addr = card[6].lstrip().rstrip()
                else: # too lazy to support uncommon base values
-                err = '[!] no street # found, possibly from base with different amount of values'
-            if addr == '': zip = '' # apparently does not support zip only, requires street if checking with addr+zip option (according to dev)
+                err = '[!] no street / zip found, possibly from base with different amount of values'
+            if addr == '': # apparently does not support zip only,
+               zip = '' # requires street if checking with addr+zip option (because of avs according to dev)
+               err = '[!] no street / zip found, possibly from base with different amount of values'
             if card[9] != '  ' and addr != '': zip = card[9].strip(' ')
-            print(f'{cc}|{exp}|{cvv}|{addr}|{zip}|{err}')
+            log = f'{cc}|{exp}|{cvv}|{addr}|{zip}|{err}'
+            if err != '': invalid += [log]
+            else: valid += [log]
+    for v in valid: print(v)
+    for iv in invalid: print(iv)
 ミ=tk.Button(ー, text="Click here to input the html source", command=ァ)
 ミ.pack();ー.mainloop()
